d62fe9ca8dd5f7d63a0f54b059477da0b4ce87e0
Данило Шаповалов <dd26@samsung.dd26.home.ixqt.local>
2020-09-15 15:58:44 +0300

    initial commit

---
+++ exercises/clippy/target/debug/deps/clippy2-9a5cf2262d3f0a6e.d
@@ -0,0 +1,5 @@
+/Users/dd26/rustlings/exercises/clippy/target/debug/deps/clippy2-9a5cf2262d3f0a6e.rmeta: clippy2.rs
+
+/Users/dd26/rustlings/exercises/clippy/target/debug/deps/clippy2-9a5cf2262d3f0a6e.d: clippy2.rs
+
+clippy2.rs:
